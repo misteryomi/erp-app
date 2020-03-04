667fdbbde59d28f63172ff0fc600e5b7aa40991e
Clicks <clicks@Progressives-MacBook-Pro.local>
2020-03-04 11:19:17 +0100

    board room booking menu

--- routes/subroutes/boardroom-booking.php
+++ routes/subroutes/boardroom-booking.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::name('boardroom-booking.')->prefix('boardroom-booking')->namespace('UtilityForms')->middleware('auth')->group(function() {
-    Route::get('/', 'BoardRoomBookingController@create')->name('create');
+    Route::get('/', 'BoardRoomBookingController@create')->name('index');
     Route::post('/', 'BoardRoomBookingController@store')->name('store');
 });
