91c3c8e957ac7f3fc23b8f7a5a9f835b38fd2594
Oluwayomi Omotoso <oomotoso@primeracredit.local>
2020-04-28 20:36:13 +0100

    f

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Spatie\Permission\PermissionServiceProvider::class,
         MikeMcLin\WpPassword\WpPasswordProvider::class,
         Kris\LaravelFormBuilder\FormBuilderServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
 
         /*
@@ -231,6 +232,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'FormBuilder' => Kris\LaravelFormBuilder\Facades\FormBuilder::class
 
     ],
